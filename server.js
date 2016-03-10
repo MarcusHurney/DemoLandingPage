a76fe863e4a9620ab4b194758d7d5ecd433ca4b4
Marcus Hurney <marcushurney@gmail.com>
2016-03-10 21:52:31 +0700

    Changes script tag to use http instead of https. Changes logo size in footer

--- server.js
+++ server.js
@@ -7,18 +7,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.use(function (req, res, next){
-	if (req.headers['x-forwarded-proto'] === 'http') {
-
-		next();
-
-	} else {
-
-		res.redirect('http://' + req.hostname + req.url);
-		
-	}
-});
-
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
